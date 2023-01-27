9c432edd78ba08bcf29486dc58047fffad12454c
redpanda185 <thegreenstreakingbox@gmail.com>
2023-01-27 12:41:20 -0800

    Added new optional parameters for the Wrapper Script
    New parameters: reduction,
      dims.use,
      theta,
      lambda,
      sigma,
      nclust,
      tau,
      block.size,
      max.iter.harmony,
      max.iter.cluster,
      epsilon.cluster,
      epsilon.harmony,
      plot_convergence,
      verbose,
      reference_values,
      reduction.save,
      assay.use,
      project.dim

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM satijalab/seurat
+FROM satijalab/seurat:4.1.0
 
 #RUN useradd -ms /bin/bash gpuser
 USER root
@@ -9,10 +9,10 @@ RUN mkdir /src
 RUN mkdir /testdata
 
 #USER gpuser
-RUN Rscript  -e "install.packages('devtools')"
-RUN Rscript  -e "install.packages('harmony')"
-RUN Rscript  -e "install.packages('optparse')"
-RUN Rscript  -e "install.packages('xfun')"
-RUN Rscript  -e "install.packages('dplyr')"
+RUN Rscript  -e "install.packages('devtools', version = '2.4.5')"
+RUN Rscript  -e "install.packages('harmony', version = '0.1.1')"
+RUN Rscript  -e "install.packages('optparse', version = '1.7.3')"
+RUN Rscript  -e "install.packages('xfun', version = '0.36')"
+RUN Rscript  -e "install.packages('dplyr', version = '1.0.10')"
 
 COPY src/* /opt/genepatt/src/
--- src/Harmony_Wrapper_Script.R
+++ src/Harmony_Wrapper_Script.R
@@ -12,16 +12,90 @@ parser <- add_option(parser, c("--file_list"),
 parser <- add_option(parser, c("--output_file"), type = 'character',
                      help = "Name of the file to save the data to", 
                      default = "harmonized_data")
-parser <- add_option(parser, c("--max_iterations"),
-                     help = "Maximum number of iterations that Harmony will run")
 parser <- add_option(parser, c("--cell_types"),
                      help = "Names of the cell types present in the dataset, separated by commas",
                      default = NULL)
 parser <- add_option(parser, c("--colors"),
                      help = "The colors you want to use to label your cell types, separated by commas",
                      default = NULL)
-# parser <- add_option(parser, c("--number of cells"),
-#                      help = "Number of cells per cell types (must be in the same order)")
+# =================================================
+# Advanced options
+parser <- add_option(parser, c("--reduction"),
+                     help = "Name of dimension reduction to use. Default is PCA",
+                     default = 'pca')
+parser <- add_option(parser, c("--dims.use"),
+                     help = "Which PCA dimensions to use for Harmony. By default, use all",
+                     default = NULL)
+parser <- add_option(parser, c("--theta"),
+                     help = "Diversity clustering penalty parameter. Specify for each variable 
+                     in group.by.vars. Default theta=2. theta=0 does not encourage any 
+                     diversity. Larger values of theta result in more diverse clusters",
+                     default = NULL)
+parser <- add_option(parser, c("--lambda"),
+                     help = "Ridge regression penalty parameter. Specify for each variable
+                     in group.by.vars. Default lambda=1. Lambda must be strictly positive.
+                     Smaller values result in more aggressive correction.",
+                     default = NULL)
+parser <- add_option(parser, c("--sigma"),
+                     help = "Width of soft kmeans clusters. Default sigma=0.1. Sigma scales
+                     the distance from a cell to cluster centroids. Larger values of sigma result
+                     in cells assigned to more clusters. Smaller values of sigma make soft
+                     kmeans cluster approach hard clustering.",
+                     default = 0.1)
+parser <- add_option(parser, c("--nclust"),
+                     help = "Number of clusters in model. nclust=1 equivalent to simple
+                     linear regression.",
+                     default = NULL)
+parser <- add_option(parser, c("--tau"),
+                     help = "Protection against overclustering small datasets with large ones.
+                     tau is the expected number of cells per cluster.",
+                     default = 0)
+parser <- add_option(parser, c("--block.size"),
+                     help = "What proportion of cells to update during clustering.
+                     Between 0 to 1, default 0.05. Larger values may be faster but less accurate",
+                     default = 0.05)
+parser <- add_option(parser, c("--max.iter.harmony"),
+                     help = "Maximum number of iterations that Harmony will run",
+                     type = 'integer',
+                     default = 10)
+parser <- add_option(parser, c("--max.iter.cluster"),
+                     help = "Maximum number of rounds to run clustering at each
+                     round of Harmony.",
+                     type = 'integer',
+                     default = 20)
+parser <- add_option(parser, c("--epsilon.cluster"),
+                     help = "Convergence tolerance for clustering round of Harmony.
+                     Set to -Inf to never stop early.",
+                     default = 1e-5)
+parser <- add_option(parser, c("--epsilon.harmony"),
+                     help = "Convergence tolerance for Harmony. Set to -Inf to
+                     never stop early.",
+                     default = 1e-4)
+parser <- add_option(parser, c("--plot_convergence"),
+                     help = "Whether to print the convergence plot of the
+                     clustering objective function. TRUE to plot, FALSE to suppress. This can be
+                     useful for debugging.",
+                     default = FALSE)
+parser <- add_option(parser, c("--verbose"),
+                     help = "Whether to print progress messages. TRUE to print, FALSE to
+                     suppress.",
+                     default = TRUE)
+parser <- add_option(parser, c("--reference_values"),
+                     help = "Defines reference dataset(s). Cells
+                     that have batch variables values matching reference_values will not be moved",
+                     default = NULL)
+parser <- add_option(parser, c("--reduction.save"),
+                     help = "Keyword to save Harmony reduction. Useful if you want
+                     to try Harmony with multiple parameters and save them as e.g.
+                     'harmony_theta0', 'harmony_theta1', 'harmony_theta2'",
+                     default = "harmony")
+parser <- add_option(parser, c("--assay.use"),
+                     help = "Which assay to run PCA on if no PCA present?",
+                     default = NULL)
+parser <- add_option(parser, c("--project.dim"),
+                     help = "Project dimension reduction loadings. Default TRUE.",
+                     default = TRUE)
+
 
 print('================================================')
 args <- parse_args(parser)
@@ -35,23 +109,21 @@ lines = readLines(con)
 panels = list(NA)
 
 namelist = args$cell_types
+#Checks if the number of cell types matches the number of files given.
 if(!is.null(namelist)){
   namelist <- strsplit(namelist, split = ',')
   if(length(namelist[[1]]) != length(lines)) stop("Number of cell types does not match number of files")
   lapply(namelist, trimws)
 }
+
 colorlist = args$colors
+#Checks if the number of colors matches the number of files given.
 if(!is.null(colorlist)){
   colorlist <- strsplit(colorlist, split = ',')
   if(length(colorlist[[1]]) != length(lines)) stop("Number of colors does not match number of cell types")
 }
 
-max_iter = args$max_iterations
-if(typeof(max_iter) == "character"){
-  max_iter <- as.numeric(max_iter)
-}
-i = 1
-
+#Reads in the files from file_list
 for (line in lines) {
   print("About to read")
   print(line)
@@ -83,30 +155,58 @@ for (line in lines) {
     i = i + 1
   }
 }
-run_harmony <- function(datalist, max_iterations){
+
+#Runs Harmony on the given data and outputs the processed data in a Seurat object.
+run_harmony <- function(datalist, args){
   data <- datalist[[1]]
   metadata_list <- NULL
+  #Merges all the Seurat objects into one object, while keeping the pca data. 
   for(x in 1:length(datalist)){
     if(x != 1){
       data <- merge(data, datalist[[x]], merge.dr = "pca", merge.data = TRUE)
     }
     metadata_list <- c(metadata_list, rep(names(datalist)[x], ncol(datalist[[x]])))
   }
+  #Sets the metadata of the Seurat object to describe the cell type from which the data originates.
   data$celltype <- metadata_list
+  #Runs all of the pre-processing steps prior to running Harmony
   data <- data %>%
     Seurat::NormalizeData(verbose = FALSE) %>%
     FindVariableFeatures(selection.method = "vst", nfeatures = 2000) %>%
     ScaleData(verbose = FALSE) %>%
     RunPCA(pc.genes = pbmc@var.genes, npcs = 20, verbose = FALSE)
   harmonizedData <- data %>%
-    RunHarmony(group.by = "celltype", plot_convergence = TRUE, max.iter.harmony = max_iterations)
+    RunHarmony(group.by = "celltype", 
+               reduction = args$reduction,
+               dims.use = args$dims.use,
+               theta = args$theta,
+               lambda = args$lambda,
+               sigma = args$sigma,
+               nclust = args$nclust,
+               tau = args$tau,
+               block.size = args$block.size,
+               max.iter.harmony = args$max.iter.harmony,
+               max.iter.cluster = args$max.iter.cluster,
+               epsilon.cluster = args$epsilon.cluster,
+               epsilon.harmony = args$epsilon.harmony,
+               plot_convergence = args$plot_convergence,
+               verbose = args$verbose,
+               reference_values = args$reference_values,
+               reduction.save = args$reduction.save,
+               assay.use = args$assay.use,
+               project.dim = args$project.dim
+               )
   return(harmonizedData)
 }
+
+#Saves a Seurat object to an RDS file
 save_it <- function(object, fileName){
   saveRDS(object, file = fileName)
   print("Saved file!")
   return(object)
 }
+
+#Produces a scatterplot of the harmonized data and saves it as a png file
 visualize <- function(harmonyObj, colors){
   print("Making plot!")
   if(is.null(colors)){
@@ -124,6 +224,7 @@ visualize <- function(harmonyObj, colors){
   dev.off()
   print("Plot made and saved!")
 }
-output <- run_harmony(panels, max_iter)
+
+output <- run_harmony(panels, args)
 save_it(output, paste(args$output_file, '.rds', sep = ''))
 visualize(output, colorlist[[1]])
